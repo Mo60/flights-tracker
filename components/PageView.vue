626168a8d57235ef0e826abed89a3e2271929dfa
Mo Khalil <51033011+Mo60@users.noreply.github.com>
2025-09-27 13:19:39 -0500

    Update PageView.vue
    Add select hour/min start/end

--- components/PageView.vue
+++ components/PageView.vue
@@ -906,34 +906,154 @@ startTime()
 
 
 
-      <p>
+      <p> Start
       <select v-model="user.selectedStartHour">
           <option disabled="">
             Choose Date
           </option>
+          <option :value="4">
+            4
+          </option>
+          </option>
+          <option :value="5">
+            5
+          </option>
+          </option>
+          <option :value="6">
+            6
+          </option>
+          <option :value="7">
+            7
+          </option>
+          <option :value="8">
+            8
+          </option>
+          <option :value="9">
+            9
+          </option>
+          <option :value="10">
+            10
+          </option>
           <option :value="11">
             11
           </option>
+          <option :value="12">
+            12
+          </option>
+          <option :value="13">
+            13
+          </option>
+          <option :value="14">
+            14
+          </option>
+          <option :value="15">
+            15
+          </option>
+          <option :value="16">
+            16
+          </option>
+          <option :value="17">
+            27
+          </option>
+          <option :value="18">
+            18
+          </option>
+          <option :value="19">
+            19
+          </option>
+          <option :value="20">
+            20
+          </option>
+          <option :value="21">
+            21
+          </option>
+          <option :value="22">
+            22
+          </option>
+
         </select>
 
     <select v-model="user.selectedStartMin">
           <option disabled="">
             Choose Date
           </option>
-          <option :value="11">
+          <option :value="0">
             0
           </option>
+          <option :value="15">
+            15
+          </option>
+          <option :value="45">
+            45
+          </option>
+          
         </select>
       </p>
 
 
-      <p>
+      <p> End
       <select v-model="user.selectedEndHour">
           <option disabled="">
             Choose Date
           </option>
+          <option :value="4">
+            4
+          </option>
+          </option>
+          <option :value="5">
+            5
+          </option>
+          </option>
+          <option :value="6">
+            6
+          </option>
+          <option :value="7">
+            7
+          </option>
+          <option :value="8">
+            8
+          </option>
+          <option :value="9">
+            9
+          </option>
+          <option :value="10">
+            10
+          </option>
           <option :value="11">
+            11
+          </option>
+          <option :value="12">
+            12
+          </option>
+          <option :value="13">
+            13
+          </option>
+          <option :value="14">
             14
+          </option>
+          <option :value="15">
+            15
+          </option>
+          <option :value="16">
+            16
+          </option>
+          <option :value="17">
+            27
+          </option>
+          <option :value="18">
+            18
+          </option>
+          <option :value="19">
+            19
+          </option>
+          <option :value="20">
+            20
+          </option>
+          <option :value="21">
+            21
+          </option>
+          <option :value="22">
+            22
           </option>
         </select>
 
@@ -941,8 +1061,14 @@ startTime()
           <option disabled="">
             Choose Date
           </option>
-          <option :value="11">
+          <option :value="0">
             0
+          </option>
+          <option :value="15">
+            15
+          </option>
+          <option :value="45">
+            45
           </option>
         </select>
       </p>
